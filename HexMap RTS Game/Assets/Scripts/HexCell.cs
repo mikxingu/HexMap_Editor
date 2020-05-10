f073283a0e1803331a10d4a5d1c0f05f502dd95d
Mike Home <michelalvs@yahoo.com.br>
2020-05-10 01:28:35 -0300

    Finished part 1 of the HexMap Tutorial. Implemented the hexagonal grid creation, grid coloration and selection method. Also, added a UI for color selection.

---
+++ HexMap RTS Game/Assets/Scripts/HexCell.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+	public HexCoordinates coordinates;
+
+	public Color color;
+}
--- HexMap RTS Game/Assets/Scripts/HexGrid.cs
+++ HexMap RTS Game/Assets/Scripts/HexGrid.cs
@@ -3,12 +3,17 @@ using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour
 {
+	public Color defaultColor = Color.white;
+	public Color touchedColor = Color.magenta;
+
 	public int width = 6;
 	public int height = 6;
 
 	public HexCell cellPrefab;
 
 	HexCell[] cells;
+
+	HexMesh hexMesh;
 
 	public Text cellLabelPrefab;
 
@@ -17,6 +22,9 @@ public class HexGrid : MonoBehaviour
 	void Awake()
 	{
 		gridCanvas = GetComponentInChildren<Canvas>();
+
+		hexMesh = GetComponentInChildren<HexMesh>();
+
 		cells = new HexCell[height * width];
 
 		for (int z = 0, i = 0; z < height; z++)
@@ -31,17 +39,37 @@ public class HexGrid : MonoBehaviour
 	void CreateCell(int x, int z, int i)
 	{
 		Vector3 position;
-		position.x = x * 10f;
+		position.x = (x + z * 0.5f - z / 2)  * (HexMetrics.innerRadius * 2f);
 		position.y = 0f;
-		position.z = z * 10f;
+		position.z = z * (HexMetrics.outerRadius * 1.5f);
 
 		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
 		cell.transform.SetParent(transform, false);
 		cell.transform.localPosition = position;
+		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+		cell.color = defaultColor;
 		Text label = Instantiate<Text>(cellLabelPrefab);
 		label.rectTransform.SetParent(gridCanvas.transform, false);
 		label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
-		label.text = x.ToString() + "\n" + z.ToString();
+		label.text = cell.coordinates.ToStringOnSeparateLines();
 	
+	}
+
+	void Start()
+	{
+		hexMesh.Triangulate(cells);
+	}
+
+
+	
+
+	public void ColorCell(Vector3 position, Color color)
+	{
+		position = transform.InverseTransformPoint(position);
+		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+		HexCell cell = cells[index];
+		cell.color = color;
+		hexMesh.Triangulate(cells);
 	}
 }
