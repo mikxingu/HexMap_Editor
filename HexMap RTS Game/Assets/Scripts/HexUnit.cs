25b49fe8dfb39f22f94ab1586ede0e6ced7c136e
Michel Alves <michelalvs@yahoo.com.br>
2020-07-01 19:11:53 -0300

    Finished Part 19 - Updated ReadME and Screenshots - Tested the functionality

--- HexMap RTS Game/Assets/Scripts/HexUnit.cs
+++ HexMap RTS Game/Assets/Scripts/HexUnit.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 
 public class HexUnit : MonoBehaviour
 {
+	List<HexCell> pathToTravel;
+
 	public static HexUnit unitPrefab;
+
+
 	HexCell location;
+
+	const float travelSpeed = 4f;
+	const float rotationSpeed = 180f;
+
 	public HexCell Location
 	{
 		get
@@ -66,7 +76,122 @@ public class HexUnit : MonoBehaviour
 	{
 		return !cell.IsUnderwater && !cell.Unit;
 	}
-		
+	
+	public void Travel (List<HexCell> path)
+	{
+		Location = path[path.Count - 1];
+		pathToTravel = path;
+		StopAllCoroutines();
+		StartCoroutine(TravelPath());
+	}
 
+	IEnumerator TravelPath()
+	{
+		Vector3 a, b, c = pathToTravel[0].Position;
+		transform.localPosition = c;
+		yield return LookAt(pathToTravel[1].Position);
+
+		float t = Time.deltaTime * travelSpeed;
+		for (int i = 1; i < pathToTravel.Count; i++)
+		{
+			a = c;
+			b = pathToTravel[i - 1].Position;
+			c = (b + pathToTravel[i].Position) * 0.5f;
+			for (; t < 1f; t += Time.deltaTime * travelSpeed)
+			{
+				transform.localPosition = Bezier.GetPoint(a, b, c, t);
+				Vector3 d = Bezier.GetDerivative(a, b, c, t);
+				d.y = 0f;
+				transform.localRotation = Quaternion.LookRotation(d);
+				yield return null;
+			}
+			t -= 1f;
+		}
+
+		a = c;
+		b = pathToTravel[pathToTravel.Count - 1].Position;
+		c = b;
+		for (; t < 1f; t += Time.deltaTime * travelSpeed)
+		{
+			transform.localPosition = Bezier.GetPoint(a, b, c, t);
+			Vector3 d = Bezier.GetDerivative(a, b, c, t);
+			d.y = 0f;
+			transform.localRotation = Quaternion.LookRotation(d);
+			yield return null;
+		}
+
+		transform.localPosition = location.Position;
+		orientation = transform.localRotation.eulerAngles.y;
+
+		ListPool<HexCell>.Add(pathToTravel);
+		pathToTravel = null;
+	}
+
+	void OnEnable()
+	{
+		if (location)
+		{
+			transform.localPosition = location.Position;
+		}
+	}
+
+
+	#region GIZMOS FOR MOVEMENT - ENABLE FOR EDITMODE
+	/*
+	void OnDrawGizmos()
+	{
+		if (pathToTravel == null || pathToTravel.Count == 0)
+		{
+			return;
+		}
+
+		Vector3 a, b, c = pathToTravel[0].Position;
+
+		for (int i = 1; i < pathToTravel.Count; i++)
+		{
+			a = c;
+			b = pathToTravel[i - 1].Position;
+			c = (b + pathToTravel[i].Position) * 0.5f;
+			for (float t = 0f; t < 1f; t += 0.1f)
+			{
+				Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
+			}
+		}
+
+		a = c;
+		b = pathToTravel[pathToTravel.Count - 1].Position;
+		c = b;
+		for (float t = 0f; t < 1f; t += 0.1f)
+		{
+			Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
+		}
+	}
+	*/
+	#endregion
+
+	IEnumerator LookAt(Vector3 point)
+	{
+		point.y = transform.localPosition.y;
+		Quaternion fromRotation = transform.localRotation;
+		Quaternion toRotation =
+			Quaternion.LookRotation(point - transform.localPosition);
+		float angle = Quaternion.Angle(fromRotation, toRotation);
+
+		if (angle > 0f)
+		{
+			float speed = rotationSpeed / angle;
+
+			for (float t = Time.deltaTime * speed;
+				t < 1f;
+				t += Time.deltaTime * speed)
+			{
+				transform.localRotation =
+					Quaternion.Slerp(fromRotation, toRotation, t);
+				yield return null;
+			}
+			transform.LookAt(point);
+			orientation = transform.localRotation.eulerAngles.y;
+		}
+	}
 }
 
